94582057a0bdcb6aa54a19238ae8b0f9fa1b3d72
alexis00rod <alexis00rodrigo@gmail.com>
2023-01-05 07:31:18 -0300

    Configure rutas principales

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { App } from './components/index'
+import { AboutMe, MyProjects } from './pages/index'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route index element={AboutMe}/>
+        <Route path='/about-me' element={MyProjects} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
